8b1bdc6c454e49a24e534004de02d40d8d20ef14
Joliot-TSIMISARAKA <136432871+Joliot-TSIMISARAKA@users.noreply.github.com>
2024-10-03 00:22:15 +0300

    Last changes on the project

--- mortgage-repayment-calculator-main/README.md
+++ mortgage-repayment-calculator-main/README.md
@@ -32,8 +32,8 @@ Users should be able to:
 
 ### Links
 
--   Solution URL: [Add solution URL here](https://your-solution-url.com)
--   Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+-   Solution URL: [here](https://www.frontendmentor.io/solutions/vanilla-javascript-mortgage-calculator-brET6Skci_)
+-   Live Site URL: [here](https://mortgage-calculator-eta-one.vercel.app/)
 
 ## My process
 
--- mortgage-repayment-calculator-main/script.js
+++ mortgage-repayment-calculator-main/script.js
@@ -6,6 +6,9 @@ let mortgageAmount = document.getElementById("amount"),
     radios = form.querySelectorAll("input[type='radio']"),
     calculateBtn = document.getElementById("calculate"),
     errors = form.querySelectorAll(".error"),
+    amountTag = form.querySelector(".amount__tag"),
+    termTag = form.querySelector(".term__tag"),
+    interestTag = form.querySelector(".interest__tag"),
     monthlyPay = document.getElementById("monthly-pay"),
     totalPay = document.getElementById("total-pay"),
     noResultPage = document.querySelector(".result__noResult"),
@@ -18,88 +21,97 @@ let inputs = [
     [radios, errors[3]],
 ];
 
-
+let tags = [amountTag, termTag, interestTag];
 
 function checkCompletion(field) {
     if (field !== radios) {
-        return (!field.value ) ? false : true;
+        return field.value ? true : false;
     } else {
-        return (radios[0].checked || radios[1].checked)
-            ? true
-            : false;
+        return Array.from(field).some((radio) => radio.checked);
     }
 }
 
 function showErrors() {
     let flag = true;
     for (let i = 0; i < inputs.length; i++) {
-        if (checkCompletion(inputs[i][0]) === false) {
-            inputs[i][1].style.display = "block";
+        const field = inputs[i][0];
+        const error = inputs[i][1];
+
+        if (checkCompletion(field) === false) {
+            error.hidden = false;
+            if (i < 3) {
+                tags[i].classList.remove("tag--okay");
+                tags[i].classList.add("tag--error");
+            }
             flag = false;
         } else {
-            inputs[i][1].style.display = "none";
+            error.hidden = true;
+            if (i < 3) {
+                tags[i].classList.remove("tag--error");
+                tags[i].classList.add("tag--okay");
+            }
         }
     }
 
-    return (flag === true) ? true : false; 
+    return flag;
 }
 
-function checkNonNums () {
-    flag = true;
-    for(let i=0; i<inputs.length-1; i++) {
-        if(isNaN(parseFloat(inputs[i][0].value)) && checkCompletion(inputs[i][0]) === true) {
+function checkNonNums() {
+    let flag = true;
+    for (let i = 0; i < inputs.length - 1; i++) {
+        if (
+            isNaN(parseFloat(inputs[i][0].value)) &&
+            checkCompletion(inputs[i][0])
+        ) {
             flag = false;
         }
     }
-    flag === true ? null : alert("Only numbers allowed !!!");
 
-    return flag === true ? true : false;
+    if (!flag) {
+        alert("Only numbers allowed !!!");
+    }
+    return flag;
 }
 
-function calculate () {
+function calculate() {
     let p = parseFloat(mortgageAmount.value),
         year = parseFloat(mortgageTerm.value),
-        r = parseFloat(interest.value)/100/12;
+        r = parseFloat(interest.value) / 100 / 12,
         n = parseFloat(year * 12);
-    
-    if(radios[0].checked) {
-        let m = (p*(r*((1+r)**n))) / (((1+r)**n) - 1),
+
+    // The formula for calculating the monthly repayment is:
+    // M = Pr(n(1 + r)^n - 1) / ((1 + r)^n - 1)
+    // Where M is the monthly repayment, P is the mortgage amount, r is the
+    // monthly interest rate and n is the number of months
+    if (radios[0].checked) {
+        let m = (p * (r * Math.pow(1 + r, n))) / (Math.pow(1 + r, n) - 1),
             total = (year * 12 * m).toFixed(2);
         return [m.toFixed(2), total];
+        // The formula for calculating the monthly repayment for an interest only
+        // mortgage is:
+        // M = Pr / 12
     } else if (radios[1].checked) {
-        let m = (r*p)/12,
-        total = (year * 12 * m).toFixed(2);
+        let m = (r * p) / 12,
+            total = (year * 12 * m).toFixed(2);
         return [m.toFixed(2), total];
     }
 }
 
-function addThousandsSeparator(num, locale) {
-    let numFormat = new Intl.NumberFormat(locale);
-    let formatedNumber = numFormat.format(num);
-    return formatedNumber;
+function displayResults(data) {
+    const resultArray = data;
+    monthlyPay.innerHTML = `£${resultArray[0]}`;
+    totalPay.innerHTML = `£${resultArray[1]}`;
+
+    noResultPage.hidden = true;
+    resultPage.hidden = false;
 }
 
-function displayResults (data) {
-    monthlyPay.textContent = `£${addThousandsSeparator(data[0], 'en-US')}`;
-    totalPay.textContent = `£${addThousandsSeparator(data[1], 'en-US')}`;
-    mortgageAmount.value = addThousandsSeparator(parseInt(mortgageAmount.value));
-
-    noResultPage.style.display = "none";
-    resultPage.style.display = "block";
-
-}
-
-calculateBtn.addEventListener("click", function (e) {   
+calculateBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
-    let flag1 = showErrors();
-    let flag2 = checkNonNums();
+    const validInputs = showErrors() && checkNonNums();
 
-
-    if (flag1 && flag2) {
-        let result = calculate();
-        displayResults(result);
+    if (validInputs) {
+        displayResults(calculate());
     }
-})
-
-
+});
